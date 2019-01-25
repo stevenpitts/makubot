e6b23001ebd2fd3118de146a627df39cc21ebac3
Pitts <pittss@wit.edu>
2019-01-24 22:05:08 -0500

    Whole lotta changes

--- makuCommands.py
+++ makuCommands.py
@@ -1,18 +1,106 @@
 import discord
 from discord.ext import commands
+import random
+from makubot import move_emote
+import sys
+import asyncio
+import traceback
+
+def freereign():
+    async def predicate(ctx):
+        return ctx.message.guild.id in ctx.bot.free_guilds
+    return commands.check(predicate)
+
 
 class MakuCommands():
-	def __init__(self,bot):
-		self.bot = bot
-	@commands.command()
-	async def ping(self):
-		await self.bot.say("pong")
-	@commands.command()
-	async def fact(self):
-		raise NotImplementedError
-	@commands.command()
-	async def refresh(self):
-		await self.bot.refresh()
+    def __init__(self,bot):
+        self.bot = bot
+        self.bot.description = """
+Hey there! I'm Makubot!
+I'm a dumb bot made by a person who codes stuff.
+I'm currently running Python {}.
+I'm pretty barebones on any server that I wasn't explicitly made to support, sorry!
+        """.format(".".join(map(str, sys.version_info[:3])))
+        
+    @commands.command()
+    async def ping(self,ctx):
+        await ctx.send("pong")
+            
+    @commands.command()
+    @freereign()
+    async def reload(self,ctx): 
+        #https://gist.github.com/EvieePy/d78c061a4798ae81be9825468fe146be
+        try:
+            compile(open("makucommands.py", 'r').read() + '\n', "makucommands.py", 'exec')
+        except Exception:
+            await self.bot.makusu.send(traceback.format_exc())
+        else:
+            self.bot.unload_extension('makucommands')
+            try:
+                self.bot.load_extension('makucommands')
+            except Exception:
+                await self.bot.makusu.send("Dang, didn't notice the error, now can't reload. ",traceback.format_exc())
+        
+    @commands.command()
+    async def emojispam(self,ctx):
+        emoji_gen = iter(sorted(self.bot.emojis,key=lambda *args: random.random()))
+        for emoji_to_add in emoji_gen:
+            try:
+                await ctx.message.add_reaction(emoji_to_add)
+            except discord.errors.Forbidden:
+                return
+                
+    @commands.command()
+    @commands.is_owner()
+    async def perish(self,ctx):
+        self.bot.close()
+        
+    @commands.command()
+    async def move(self,ctx,msg_id,channel_to_move_to:discord.TextChannel):
+        """
+        move <message_id> <channel_mention>: move a message from the current channel to the channel specified (I need special permissions for this!) You can also add the reaction \U0001f232 to automate this process."""
+        try:
+            message_to_move = await ctx.message.channel.get_message(msg_id)
+        except discord.errors.HTTPException:
+            await ctx.message.channel.send("That, uh, doesn't look like a valid message ID. Try again.")
+        else:
+            await self.bot.move_message_attempt(message_to_move,channel_to_move_to,ctx.message.author)
+            
+    @commands.command()
+    @commands.is_owner()
+    async def gowild(self,ctx):
+        await self.bot.add_free_reign_guild(ctx.message.guild.id)
+            
+    async def on_member_join(self,member:discord.Member):
+        if member.guild.id in self.bot.free_guilds:
+            await member.guild.system_channel.send(member.mention+" Hi! Maku loves you! <333333")
+        
+        
+    async def on_reaction_add(self,reaction,user):
+        if reaction.emoji == move_emote:
+            await reaction.message.channel.send(user.mention+" Move to which channel?")
+            self.bot.move_requests_pending[user] = reaction.message
+    async def on_reaction_clear(self,message:discord.Message,reactions):
+        pass
+    async def on_member_remove(self,member:discord.Member):
+        pass
+    async def on_member_update(self,before,after):
+        pass
+    async def on_guild_join(self,guild:discord.Guild):
+        pass
+    async def on_guild_remove(self,guild:discord.Guild):
+        pass
+    async def on_guild_role_create(self,role:discord.Role):
+        pass
+    async def on_guild_emojis_update(self,guild:discord.Guild,before,after):
+        pass
+    async def on_member_ban(self,guild:discord.Guild,user):
+        pass
+    async def on_voice_state_update(self,member:discord.Member,before,after):
+        pass
+    async def on_group_join(self,channel,user):
+        pass
+        
+    
 def setup(bot):
-	bot.add_cog(MakuCommands(bot))
-	
+    bot.add_cog(MakuCommands(bot))
--- makubot.py
+++ makubot.py
@@ -1,96 +1,146 @@
 import discord
 from discord.ext import commands
 import random
-import conf
 import asyncio
-from conf import *
+import time
 import tokens
 from tokens import *
-import imp
+import re
+import sys
+
+#TODO switch superclass from bot to client cuz bot is stupid
+#Actually maybe don't?
+
+smugbotID = 194991333763579906
+log_to_file = True
+
+move_emote = "\U0001f232"
+
 
 
 
 class MakuBot(commands.Bot):
-	#So, bot is a subclass of discord.Client, and this is a subclass of bot.
-	
-	def __init__(self,command_prefix='?',description="Makusu's bot"):
-		commands.Bot.__init__(self,command_prefix=command_prefix,description=description)
-		self.startupExtensions = ["makuCommands",]
+    #So, bot is a subclass of discord.Client, and this is a subclass of bot.
 
-	def printDebugInfo(self):
-		print("Current servers:")
-		for server in self.servers:
-			print("   "+str(server))
-		#for server in self.servers: print(str(server))
-		print("Messages:")
-		for message in self.messages:
-			print("   "+message)
-		#for message in self.messages: print(str(message))
-		#print("Application info:")
-		#print(str(list(self.application_info())))
-		#for item in self.application_info():
-		#	print(item)
-	async def makePlaying(self,gameName):
-		await self.change_presence(game=discord.Game(name=gameName))
-	async def on_ready(self):
-		"""Triggered when bot connects. Event."""
-		print('Logged in as')
-		print(self.user.name)
-		print(self.user.id)
-		print('------')
-		await self.change_presence(game=discord.Game(name="Dick"))
-		for extension in self.startupExtensions:
-			try:
-				self.load_extension(extension)
-			except Exception as e:
-				exc = '{}: {}'.format(type(e).__name__, e)
-				print('Failed to load extension {}\n{}'.format(extension, exc))
-		self.printDebugInfo()
-		#self.command_prefix = self.user.mention
-			
-	async def on_message(self,message : discord.Message):
-		if message.author != self.user:
-			#print("on_message called. Author: "+message.author.name+". Message: "+message.content)
-			if message.mention_everyone:
-				await self.send_message(message.channel,content=message.author.mention+" did you just do the thing.")
-			if "vore" in message.content:
-				#await bot.add_reaction(message,364133071521447939
-				await self.pin_message(message)
-			if (message.author.bot or (message.author.name.lower() == "smugbot")):
-				await self.send_message(message.channel,content="Wow, apparently I'm not a good enough bot for you guys")
-			if self.user in message.mentions or message.server.me in message.mentions:
-				await self.makePlaying(message.author.name)
-		await bot.process_commands(message)
-		#That's for some thing with the API, it's weird but don't remove it
-	async def on_message_delete(self,message):
-		print(getMessageString(message))
-		await self.process_commands(message)
-		#That's for some thing with the API, it's weird but don't remove it\
-	async def on_message_edit(self,before,after):
-		probMisspelledWord = getOriginalWord(before.content,after.content)
-		if not probMisspelledWord is None:
-			await self.send_message(before.channel,content="LOL nice going there with your '"+probMisspelledWord+"'")
-	
-	async def refresh(self):
-		for extension in self.startupExtensions:
-			self.unload_extension(extension)
-			self.load_extension(extension)
-		
-	
-bot = MakuBot()
+    def __init__(self,correct_typos=False):
+        self.correct_typos = correct_typos
+        commands.Bot.__init__(self,command_prefix=commands.when_mentioned,description="Makusu's bot",case_insensitive=True,owner_id=203285581004931072)
+        self.move_requests_pending = {}
+        self.lastTime = time.time()
+        self.makusu = None
+        self.free_guilds = None
 
+    def printDebugInfo(self):
+        print("Current servers: ",{guild.name:guild.id for guild in self.guilds})
+        
 
+    async def on_ready(self):
+        """Triggered when bot connects. Event."""
+        print('Logged in as ',self.user.name,' with ID ',self.user.id)
+        self.makusu = await self.get_user_info(self.owner_id)
+        await self.change_presence(activity=discord.Game(name=r"SmugBot is being tsun to me :<"))
+        self.load_extension('makucommands')
+        await self.load_free_reign_guilds()
+        print("blac",self.free_guilds)
+        self.printDebugInfo()
 
+    async def on_message(self,message : discord.Message):
+        if message.author != self.user:
+             if message.mention_everyone:
+                 await message.channel.send(message.author.mention+" grr")
+             if "vore" in message.content.split():
+                 await message.pin()
+             if self.user in message.mentions:
+                 await self.change_presence(activity=discord.Game(name=message.author.name))
+             if "maku" in message.content.lower() and r"@ma" not in message.content.lower() and "makubot" not in message.content.lower():
+                 await message.channel.send(r"<@!203285581004931072>")
+             
+             if message.author in self.move_requests_pending:
+                 try:
+                     channel_id = int(message.content.strip().replace("<","").replace("#","").replace(">",""))
+                     channel_to_move_to = self.get_channel(channel_id)
+                 except ValueError:
+                     await message.channel.send("That doesn't look like a tagged channel, try again. (You do not need to readd the reaction. Type \"cancel\" to cancel the move request.)")
+                 except TypeError:
+                    await message.channel.send("Hmmm, that looks like a channel but I can't figure out what it is. It's already been logged for Maku to debug.")
+                    print("Couldn't figure out what channel "+str(channel_id)+" was.")
+                 else:
+                    message_to_move = self.move_requests_pending.pop(message.author)
+                    asyncio.get_event_loop().create_task(self.move_message_attempt(message_to_move,channel_to_move_to,message.author))
+                     
+
+        await self.process_commands(message) #That's for some thing with the API, it's weird but don't remove it
+        
+    async def on_message_delete(self,message):
+        deletion_message = "A user has deleted a message. "+str(getMessageString(message))
+        for attachment in message.attachments:
+            try:
+                await attachment.save(r"saved_attachments\attch"+str(random.randint(0,100000000)))
+            except discord.errors.Forbidden:
+                deletion_message += "Could not save attachment from {} in {} due to it being deleted".format(message.author,message.channel)
+        if log_to_file:
+            with open("mylog01.txt","a") as f:
+                f.write(deletion_message+"\n")
+        else:
+            print(deletion_message)
+        await self.process_commands(message)
+        #That's for some thing with the API, it's weird but don't remove it
+    async def on_message_edit(self,before,after):
+        if self.correct_typos:
+            probMisspelledWord = getOriginalWord(before.content,after.content)
+            if not probMisspelledWord is None:
+                if (time.time() - self.lastTime > 100):
+                    await before.channel.send("LOL nice going there with your '"+probMisspelledWord+"'")
+                    self.lastTime = time.time()
+                
+    
+    async def move_message_attempt(self,message:discord.Message, channel:discord.TextChannel, move_request_user:discord.member.Member):
+        member_can_manage_messages = channel.permissions_for(move_request_user).manage_messages
+        if member_can_manage_messages or move_request_user == message.author:
+            if message.attachments:
+                await message.channel.send("That guy has attachments which'd be deleted. Maku is adding support for that soon.")
+            else:
+                new_message_content = "{} has moved this here from {}. OP was {}.\n{}".format(move_request_user.mention,message.channel.mention,message.author.mention,message.content)
+                await channel.send(new_message_content)
+                await message.delete()
+        else:
+            await message.channel.send("Looks like you don't have the manage messages role and you're not OP. sorry.")
+        
+    async def load_free_reign_guilds(self):
+        self.free_guilds = []
+        with open('free_reign.txt','r') as f:
+            for line in f:
+                try:
+                    self.free_guilds.append(int(line))
+                except ValueError:
+                    pass
+    async def save_free_reign_guilds(self):
+        with open('free_reign.txt','w') as f:
+            for guild_id in self.free_guilds:
+                f.write("{}\n".format(guild_id))
+    async def add_free_reign_guild(self,guild_id):
+        self.free_guilds.append(guild_id)
+        with open('free_reign.txt','a') as f:
+            f.write("{}\n".format(guild_id))
+    async def remove_free_reign_guild(self,guild_id):
+        self.free_guilds.remove(guild_id)
+        await self.save_free_reign_guilds()
+    
+        
+makubot = MakuBot()
 def getMessageString(message):
-	return str(message.timestamp)+" "+message.author.name+" in "+str(message.channel)+"   "+message.content
+    return str(message.created_at)+" "+message.author.name+" in "+str(message.channel)+"   "+message.content
 def getOriginalWord(before,after):
-	"""Called for edited messages. Args of "Lol taht was funny" and "Lol that was funny" should return "taht" """
-	beforeArray = before.split()
-	afterArray = after.split()
-	for word in beforeArray:
-		if not (word in afterArray):
-			return word
+    """Called for edited messages. Args of "Lol taht was funny" and "Lol that was funny" should return "taht" """
+    return [word for word in before.split() if word not in after.split()][0]
+
+def main():
+	makubot.run(makubotToken)
+    #asyncio.get_event_loop().run_until_complete(makubot.start(makubotToken))
+
+if __name__=="__main__":
+    main()
+
 
 #add_reaction
 #Fact command that makes bot print the first sentence of a random wikipedia article
-#Make it correct people's grammar
