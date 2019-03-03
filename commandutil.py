a292133a76fc59b817a5dc86051a8371fcb538a3
Maku <makusu2@gmail.com>
2019-03-03 16:09:04 -0500

    Made flake8 passing

--- commandutil.py
+++ commandutil.py
@@ -1,9 +1,15 @@
 import traceback
 
-async def send_formatted_message(recipient,msg):
+
+async def send_formatted_message(recipient, msg):
     for i in range(0, len(msg), 1500):
         await recipient.send(r"```"+msg[i:i+1500]+r"```")
+
+
 def get_formatted_traceback(e):
     return ''.join(traceback.format_exception(type(e), e, e.__traceback__))
-    
-known_ids = {"aagshit":322509699159162883,"aagshit_lawgs":541742610604359720, "lilybots":[287343736046878730,]}
+
+
+known_ids = {"aagshit": 322509699159162883,
+             "aagshit_lawgs": 541742610604359720,
+             "lilybots": [287343736046878730]}
--- makubot.py
+++ makubot.py
@@ -1,13 +1,15 @@
 """
 Main module for makubot.
-This module should never have to be reloaded;
-    all reloading should take place in makucommands, criticalcommands, and commandutil.
+This module should never have to be reloaded.
+All reloading should take place in makucommands,
+criticalcommands, and commandutil.
 """
 import logging
 import discord
 from discord.ext import commands
 import tokens
 logging.basicConfig(filename='makubot.log', level=logging.INFO)
+
 
 class MakuBot(commands.Bot):
     '''
@@ -17,7 +19,8 @@ class MakuBot(commands.Bot):
 
     def __init__(self):
         commands.Bot.__init__(self, command_prefix=commands.when_mentioned,
-                              case_insensitive=True, owner_id=203285581004931072)
+                              case_insensitive=True,
+                              owner_id=203285581004931072)
         self.makusu = None
         self.load_extension('criticalcommands')
         self.load_extension('makucommands')
@@ -27,9 +30,11 @@ class MakuBot(commands.Bot):
         Called when MakuBot has logged in and is ready to accept commands
         '''
         self.makusu = await self.get_user_info(self.owner_id)
-        print('Logged in as {} with ID {}'.format(self.user.name, self.user.id))
-        await self.change_presence(activity=discord.Game(name=r"SmugBot is being tsun to me :<"))
+        print('Logged in as {} with ID {}'
+              .format(self.user.name, self.user.id))
+        await self.change_presence(activity=discord.Game(
+            name=r"SmugBot is being tsun to me :<"))
+
 
 if __name__ == "__main__":
     MakuBot().run(tokens.makubotToken)
-    
