65bc59bf24d263aeaeab3753e66abf1354fea0b8
Maku <makusu2@gmail.com>
2019-04-25 22:04:08 -0400

    Logging won't fail on attachment error anymore

--- project/serverlogging.py
+++ project/serverlogging.py
@@ -51,10 +51,11 @@ class ServerLogging(discord.ext.commands.Cog):
             filepath = SAVED_ATTACHMENTS_DIR / attachment.filename
             try:
                 await attachment.save(filepath, use_cached=True)
-            except discord.errors.HTTPException:
-                await attachment.save(filepath, use_cached=False)
-            except discord.errors.NotFound:
-                continue
+            except (discord.errors.HTTPException, discord.errors.NotFound):
+                try:
+                    await attachment.save(filepath, use_cached=False)
+                except (discord.errors.HTTPException, discord.errors.NotFound):
+                    continue
             attachment_files.append(discord.File(filepath))
         deletion_message = (
             f'{message.created_at}: A message from {message.author.name} '
