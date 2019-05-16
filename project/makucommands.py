2bb44dcbf5996db4c43f821222c58f70f85b875c
Maku <makusu2@gmail.com>
2019-05-16 09:45:27 -0400

    Add change to v pinning

--- project/makucommands.py
+++ project/makucommands.py
@@ -448,7 +448,8 @@ class MakuCommands(discord.ext.commands.Cog):
             if (message.guild
                     and (message.guild.id
                          in self.bot.shared['data']['free_guilds'])
-                    and 'vore' in message.content.split()):
+                    and 'vore' in message.content.split()
+                    and random.random() > 0.8):
                 await message.pin()
             if (not message.author.bot
                 and ((message.guild and self.bot.user in message.mentions)
