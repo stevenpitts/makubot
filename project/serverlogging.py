ee5baf9591dd1f9f4a3c8cadb5667a5a7e592e5d
Maku <makusu2@gmail.com>
2019-10-02 09:27:28 -0400

    Remove falty await

--- project/serverlogging.py
+++ project/serverlogging.py
@@ -124,7 +124,7 @@ class ServerLogging(discord.ext.commands.Cog):
             try:
                 await log_to_channel.send(embed=embed)
             except aiohttp.client_exceptions.ClientConnectorError:
-                await embed.set_image(url=(
+                embed.set_image(url=(
                     r"https://t7.rbxcdn.com/"
                     r"b108964694f35a0db26262e4ba1d3d86"))
                 await log_to_channel.send(embed=embed)
