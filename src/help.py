c1330343fb7a221b115439f290217f9fdc2b0627
Maku <makusu2@gmail.com>
2020-09-04 21:44:24 -0400

    Improve help command

--- src/help.py
+++ src/help.py
@@ -18,31 +18,44 @@ SHOWOFF_COMMANDS = [
     "support",
     "",
     "evaluate",
-    "bully",
-    "choose",
-    "emojispam",
-    "fact",
-    "hugme",
     "poll",
     "remindme",
     "whatis",
     "youtube"
 ]
+MINOR_SHOWOFF_COMMANDS = [
+    "bully",
+    "choose",
+    "emojispam",
+    "fact",
+    "hugme"
+]
 
 
 def cmd_help_text(bot, cmd):
     help_brief = bot.all_commands[cmd].help.split('\n')[0]
-    return f"**{cmd}**\n> {help_brief}\n"
+    return f"**mb.{cmd}**\n> {help_brief}\n"
 
 
 def get_help_commands_text(bot):
-    help_text = ""
+    text = ""
     for cmd in SHOWOFF_COMMANDS:
         if cmd:
-            help_text += cmd_help_text(bot, cmd)
+            text += cmd_help_text(bot, cmd)
         else:
-            help_text += "\n"
-    return help_text
+            text += "\n"
+    return text
+
+
+def get_minor_help_commands_text():
+    # This can probably be simplified
+    text = ""
+    for i, cmd in enumerate(MINOR_SHOWOFF_COMMANDS):
+        if len(MINOR_SHOWOFF_COMMANDS) - i == 1:
+            text += f"and `mb.{cmd}`!"
+        else:
+            text += f"`mb.{cmd}`, "
+    return f"Some other commands include {text}"
 
 
 HELP_TEXT = """
@@ -50,16 +63,15 @@ I'm Makubot! I have ***hundreds*** of community-driven image commands, \
 and you can add more!
 If you can think of an emotion, or a character, or a TV show, I probably have \
 a command for it. Try `mb.angry`, `mb.nao`, or `mb.f`!
-
 If I don't have that command, you can add an image to it by using \
 `mb.addimage`! Then my owner will approve the image, and it'll \
 get added to my collection!
-
 I'm not just another image bot; I can process lots of image/video types, \
 I'll try to show you only images relevant to you, and I'm \
 super speedy! Check me out!
-
 {help_commands_text}
+{minor_help_commands_text} \
+For detailed help on a command and how to use it, use `mb.help CommandName`!
 """
 
 
@@ -68,7 +80,8 @@ class MyHelpCommand(commands.DefaultHelpCommand):
         destination = self.get_destination()
         await destination.send(
             HELP_TEXT.format(
-                help_commands_text=get_help_commands_text(self.context.bot)
+                help_commands_text=get_help_commands_text(self.context.bot),
+                minor_help_commands_text=get_minor_help_commands_text()
             )
         )
 
