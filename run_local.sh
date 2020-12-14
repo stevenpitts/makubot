74044e2b7399bc6a5a542fbc0096bbb6839db0d0
Maku <makusu2@gmail.com>
2020-12-14 18:41:12 -0500

    Move prepare_vars logic to run_local

--- run_local.sh
+++ run_local.sh
@@ -1,17 +1,31 @@
 #!/usr/bin/env bash
 
-source ./prepare_vars.sh
+export BOT_NAME="${BOT_NAME:-makumistake}"
 
-export AWS_ACCESS_KEY_ID=${bot_access_key}
-export AWS_SECRET_ACCESS_KEY=${bot_secret_key}
-export DISCORD_BOT_TOKEN=${discord_token}
-export GOOGLE_API_KEY=${google_api_key}
-export S3_BUCKET=${s3_bucket}
-export PGPASSWORD=${pgpassword}
-export PGHOST=localhost
-export PGPORT=${db_port}
-export PGUSER=postgres
-export PGNAME=postgres
+credentials_secret_name="${credentials_secret_name:-$BOT_NAME}"
+
+credentials=$(aws secretsmanager get-secret-value \
+  --secret-id "$credentials_secret_name" \
+  --query SecretString \
+  --output text)
+
+export AWS_ACCESS_KEY_ID
+export AWS_SECRET_ACCESS_KEY
+export DISCORD_BOT_TOKEN
+export PGPASSWORD
+export GOOGLE_API_KEY
+
+AWS_ACCESS_KEY_ID=$(echo "$credentials" | jq -r ".AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY=$(echo "$credentials" | jq -r ".AWS_SECRET_ACCESS_KEY")
+GOOGLE_API_KEY=$(echo "$credentials" | jq -r ".GOOGLE_API_KEY")
+DISCORD_BOT_TOKEN=$("echo $credentials" | jq -r ".DISCORD_TOKEN")
+PGPASSWORD=$(echo "$credentials" | jq -r ".PGPASSWORD")
+
+export S3_BUCKET="${s3_bucket:-$BOT_NAME}"
+export PGHOST="${db_host:-localhost}"
+export PGPORT="${db_port:-5432}"
+export PGUSER="${db_user:-postgres}"
+export PGNAME="${db_name:-mb_db}"
 
 export DEVELOPMENT=1
 
