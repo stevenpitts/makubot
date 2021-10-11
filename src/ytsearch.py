ae8fd41ebbf741704af8e1416d00acbe3174f311
Queen Tired <43617576+msqueentired@users.noreply.github.com>
2021-10-11 17:01:21 -0500

    Update src/ytsearch.py
    Co-authored-by: Steven Pitts <25968054+makusu2@users.noreply.github.com>

--- src/ytsearch.py
+++ src/ytsearch.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 import logging
 from googleapiclient.discovery import build
-from discord_slash import SlashCommand, cog_ext
+from discord_slash import cog_ext
 
 logger = logging.getLogger()
 
